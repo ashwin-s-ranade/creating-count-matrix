cac5f0e24f9d88993964f758bf31322aaa29c510
ashwin-s-ranade <ashwinranade99@gmail.com>
2020-07-15 01:41:17 -0700

    added params.runtime variable to run.sh shell script

--- run.sh
+++ run.sh
@@ -9,5 +9,5 @@
 #insufficient RAM with 10 gigs -- trying with 50 gigs works correctly
 #requires '-pe shared' * 'h_data' data, trying 4 * 10 = 40G right now  
 #'-pe shared 4' = 4 threads
-snakemake -j 99 -p --cluster "qsub -pe shared 4 -l h_data=10G,h_rt=01:00:00 -cwd -V" 
+snakemake -j 99 -p --cluster "qsub -pe shared 4 -l h_data=10G,h_rt={params.runtime} -cwd -V" 
 mail -s "snakemake finished" asr123@ucla.edu
