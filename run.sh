a0ec2530b0d82abbe434873c22195d4963b1e319
ashwin-s-ranade <ashwinranade99@gmail.com>
2020-07-14 23:24:32 -0700

    changed memory from 40G / thread to 10G / thread

--- run.sh
+++ run.sh
@@ -6,7 +6,8 @@
 #~20min for alignment with STAR
 #~5 min for counting with featureCounts
 
-#insufficient RAM with 10 gigs -- trying with 50 gigs works correctly 
-#'-pe shared 8' = 4 threads
-snakemake -j 99 -p --cluster "qsub -pe shared 4 -l h_data=50G,h_rt=01:00:00 -cwd -V" 
+#insufficient RAM with 10 gigs -- trying with 50 gigs works correctly
+#requires '-pe shared' * 'h_data' data, trying 4 * 10 = 40G right now  
+#'-pe shared 4' = 4 threads
+snakemake -j 99 -p --cluster "qsub -pe shared 4 -l h_data=10G,h_rt=01:00:00 -cwd -V" 
 mail -s "snakemake finished" asr123@ucla.edu
