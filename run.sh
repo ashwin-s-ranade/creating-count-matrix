34b48e44f2a1384356e20dbcced8ae7db5d50867
ashwin-s-ranade <ashwinranade99@gmail.com>
2020-07-14 17:08:42 -0700

    add multi-threading flags to run.sh
    - added parallelization flags with '-p' option
    - changed threads from 8 to 4, since nodes can't handle 8 cores apparently
    - removed 'module load STAR', will be trying to do that in a modulefile on startup instead

--- run.sh
+++ run.sh
@@ -2,11 +2,11 @@
 #$ -V
 #$ -cwd
 
-#module load STAR #need to source module command, hence 'source run.sh' vs './run.sh'
 #for one sample: 
 #~20min for alignment with STAR
 #~5 min for counting with featureCounts
 
 #insufficient RAM with 10 gigs -- trying with 50 gigs works correctly 
-snakemake -j 99 -p --cluster "qsub -l h_data=50G,h_rt=01:00:00 -cwd -V" 
+#'-pe shared 8' = 4 threads
+snakemake -j 99 -p --cluster "qsub -pe shared 4 -l h_data=50G,h_rt=01:00:00 -cwd -V" 
 mail -s "snakemake finished" asr123@ucla.edu
